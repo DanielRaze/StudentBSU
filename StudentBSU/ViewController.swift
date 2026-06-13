15e044a5b5ee6fdb755e6dee258e08f60fea07cd
Daniel Makaravets <danmak986@gmail.com>
2026-06-13 15:10:05 +0300

    Initial Commit

---
+++ StudentBSU/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudentBSU
+//
+//  Created by Daniel Makaravets on 13.06.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
